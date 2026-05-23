758da6cd5be4c6ca0e2d51a3b8a4bb13b67a9b1d
Mafaz03 <mohdmafaz200303@gmail.com>
2026-05-23 14:28:46 +0530

    device fix

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ def scaled_dot_product_attention(
     # scores = torch.matmul(Q, KT)# / math.sqrt(d_k)          # (B, H, seq_q, seq_k)
     
     if mask is not None: 
-        scores = scores.masked_fill(mask, float('-inf'))
+        scores = scores.masked_fill(mask, float('-inf')).to(scores.device)
 
     attn_weights = torch.softmax(scores, dim=-1)            # (B, H, seq_q, seq_k)
     if dropout is not None:
