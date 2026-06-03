bf6ef4a4fe658523b10a5f46b0498f9bdc9b5b9b
Mafaz03 <mohdmafaz200303@gmail.com>
2026-06-03 16:11:19 +0530

    typo fixed

--- config.py
+++ config.py
@@ -29,14 +29,14 @@ TEST_BATCH_SIZE  = 8
 EPOCHS           = 500
 
 # dimensions
-GRID_SIZE  = 6
+GRID_SIZE  = 64
 PATCH_SIZE = 8
 PATCH_DIM  = (PATCH_SIZE * PATCH_SIZE * C) + FOURIER_DIMENSIONS # +64 because positional embedding was done in the dataset itself
                                                                 # tgt: (patch_row * patch_col, C * patch_h * patch_w + (2 * 2 * num_freq))
 D_MODEL    = 1024
 
 # parameters
-N         = 10
+N         = 6
 NUM_HEADS = 8
 D_FF      = 1024
 DROPOUT   = 0.01
